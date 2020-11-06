2a1ce4f72de07d042a58e26aed3ae513b4d50be1
nilutpal <nilutpal.ee@gmail.com>
2020-11-06 18:32:55 +0530

    final commit

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,14 +1,28 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+import {GlobalContext} from "../context/GlobalState"; 
+
 
 export const AddTransaction = () => {
   // in form we need some component level state.
   const [text, setText] = useState();
   const [amount, setAmount] = useState(0);
 
+  const {addTransaction} = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount
+    }
+    addTransaction(newTransaction);
+  }
+
   return (
     <React.Fragment>
       <h3>Add new Transaction</h3>
-      <form id="form">
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" value = {text} onChange={(e) => setText(e.target.value)}placeholder="Enter text ..."></input>
